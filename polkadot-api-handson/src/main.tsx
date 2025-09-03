7d2956dd31dc1c4c4aa2b373a24d378ee228d33d
nasihudeen04@gmail.com <nasihudeen04@gmail.com>
2025-09-03 11:01:58 +0800

    observable handson

--- polkadot-api-handson/src/main.tsx
+++ polkadot-api-handson/src/main.tsx
@@ -1,57 +1,74 @@
+import { pas,dot } from "@polkadot-api/descriptors";
+import { createClient } from "polkadot-api";
+import { chainSpec } from "polkadot-api/chains/polkadot";
+import { getSmProvider } from "polkadot-api/sm-provider";
+import type { JsonRpcProvider } from "polkadot-api/ws-provider/web";
+import { start } from "polkadot-api/smoldot";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { createClient, type SS58String } from "polkadot-api";
-import { getSmProvider } from "polkadot-api/sm-provider";
-import { start } from "polkadot-api/smoldot";
-import { chainSpec } from "polkadot-api/chains/paseo";
-import { dot } from "@polkadot-api/descriptors";
+import "./index.css";
 
 const smoldot = start();
-
-const relayChain = smoldot.addChain({
+const chain = smoldot.addChain({
   chainSpec,
 });
 
-const client = createClient(getSmProvider(relayChain));
-const typedApi = client.getTypedApi(dot);
+const smoldotProvider = getSmProvider(chain);
 
-const res = await typedApi.query.System.Account.getValue(
-  "5FxrUu1PUugUYs6HQ83bDswjGLyHYTEzm7yqmrkKVPaYe71Y"
-);
-console.log("my account", res);
+const provider: JsonRpcProvider = (onMsg) => {
+  const smoldotConnection = smoldotProvider((msg) => {
+    console.log(JSON.parse(msg));
+    onMsg(msg);
+  });
 
-typedApi.query.System.Events.watchValue().subscribe((events) => {
-  console.log(events);
+  return {
+    send(message) {
+      console.log(JSON.parse(message));
+      smoldotConnection.send(message);
+    },
+    disconnect() {
+      smoldotConnection.disconnect();
+    },
+  };
+};
+
+let id = 0;
+
+const connection = provider((msg) => {
+  const parsedMsg = JSON.parse(msg);
+
+  // Figure out follow subscription
+  if (parsedMsg.result && !parsedMsg.method) {
+    followSubscriptionId = parsedMsg.result;
+  } else if (parsedMsg.method === "chainHead_v1_followEvent" && 
+            parsedMsg.params.result.event === "stop") {
+    sendFollow();
+  }
 });
 
-async function getSudoAccountFreeBalance(): Promise<{ balance: bigint; account: string }> {
-  const sudoAccount = await client.getUnsafeApi().query.Sudo.Key.getValue();
-  const accountInfo = await typedApi.query.System.Account.getValue(sudoAccount);
-  return { balance: accountInfo.data.free, account: sudoAccount };
+function getBody(blockHash: string) {
+  connection.send(
+    JSON.stringify({
+      jsonrpc: "2.0",
+      id: ++id,
+      method: "chainHead_v1_body",
+      params: [followSubscriptionId, blockHash],
+    })
+  );
 }
 
-getSudoAccountFreeBalance().then((result) => {
-  console.log("Account info:", result.account);
-
-  console.log("Balance:", result.balance);
-});
-
-async function findAllProxyAccountsOfTypeAny(): Promise<Array<SS58String>> {
-  const result = await typedApi.query.Proxy.Proxies.getEntries();
-  return result.map(entry => entry.keyArgs[0]);
+function sendFollow() {
+  connection.send(
+    JSON.stringify({
+      jsonrpc: "2.0",
+      id: ++id,
+      method: "chainHead_v1_follow",
+      params: [true],
+    })
+  );
 }
-
-findAllProxyAccountsOfTypeAny().then(accounts => console.log("I was able to get these proxy accounts:", accounts));
-
-async function findMaxProxyAccounts(): Promise<Array<SS58String>> {
-  const maxDelegates = await typedApi.constants.Proxy.MaxProxies();
-  const result = await typedApi.query.Proxy.Proxies.getEntries();
-  return result.filter(entry => entry.value.length === maxDelegates).map(entry => entry.keyArgs[0]);
-}
-
-findMaxProxyAccounts().then(accounts => console.log("I was able to get these proxy accounts:", accounts));
+sendFollow();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ polkadot-api-handson/src/rxj-tests.ts
@@ -0,0 +1,35 @@
+import { Observable, map, interval } from "rxjs";
+
+const take =
+  <T, R>(amount: number) =>
+  (source: Observable<T>) =>
+    new Observable<R>((subscriber) => {
+      let count = 0;
+
+      const subscription = source.subscribe({
+        next: (v) => {
+          if (count < amount) {
+            subscriber.next(v);
+            count++;
+            if(count === amount) {
+              subscriber.complete();
+            }
+          }
+        },
+        error: (e) => subscriber.error(e),
+        complete: () => subscriber.complete(),
+      });
+
+      return () => {
+        subscription.unsubscribe();
+      };
+    });
+
+const multipliedBy2$ = interval(200).pipe(
+  map((v) => v * 2),
+  take(10)
+);
+
+multipliedBy2$.subscribe((r) => {
+  console.log(r);
+});
